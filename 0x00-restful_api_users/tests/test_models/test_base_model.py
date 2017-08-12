08062f4028ddf7cfc19346082f6f918956f5e0f5
sravanthisinha <sravani.sinha@gmail.com>
2017-08-12 12:57:56 -0700

    tesk 1

---
+++ 0x00-restful_api_users/tests/test_models/test_base_model.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+"""
+This is the test_base_model to test base_model module.
+This is a TestBaseModel class inside the test_base_model module.
+"""
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    This is a TestBaseModel class
+    """
+
+    def setUp(self):
+        """
+        create the object base_model
+        """
+        self.base_model = BaseModel()
+
+    def test___init__(self):
+        """
+        method to check if instance initializes
+        """
+        self.assertIsNotNone(self.base_model)
+
+    def test_nones(self):
+        """
+        Test if id, created_at, updated_at has a value.
+        """
+        self.assertIsNotNone(self.base_model.id)
+        self.assertIsNotNone(self.base_model.created_at)
+        self.assertIsNotNone(self.base_model.updated_at)
+
+    def test_attributes(self):
+        """
+        method to test if basemodel has attributes id, created_at, updated_at
+        """
+        self.assertTrue(hasattr(self.base_model, "created_at"))
+        self.assertTrue(hasattr(self.base_model, "updated_at"))
+        self.assertTrue(hasattr(self.base_model, "id"))
+
+    def test_unique_id(self):
+        """
+        Test if ids are unique.
+        """
+        base_model_2 = BaseModel()
+        self.assertNotEqual(self.base_model.id, base_model_2.id)
+
+    def tearDown(self):
+        """
+        dispose the object base_model
+        """
+        self.base_model = None
+
+
+if __name__ == "__main__":
+    main()
