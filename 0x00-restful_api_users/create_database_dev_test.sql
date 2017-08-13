0e29bc62d5dc96d47fc8a38f7defb383465a2509
sravanthisinha <sravani.sinha@gmail.com>
2017-08-12 17:37:38 -0700

    task 8

---
+++ 0x00-restful_api_users/create_database_dev_test.sql
@@ -0,0 +1,5 @@
+-- creates the databases hbtn_yelp_dev, hbtn_yelp_test
+-- creates the databases hbtn_yelp_dev if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_yelp_dev;
+-- creates the databases hbtn_yelp_test if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_yelp_test;
