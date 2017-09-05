9a3aa1b372067836a281ed1e6c76af81c40496d1
sravanthisinha <sravani.sinha@gmail.com>
2017-09-05 16:35:49 -0700

    finish task 6

--- 0x01-restful_api_users/api/v1/app.py
+++ 0x01-restful_api_users/api/v1/app.py
@@ -3,11 +3,23 @@ from flask import jsonify
 from os import getenv
 from api.v1.views import app_views
 
+
 env_port = getenv('HBNB_API_PORT')
 env_host = getenv('HBNB_API_HOST')
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+     error handler for 404 error
+    :param e:
+
+    """
+    return jsonify(error="Not found"), 404
+
 
 if __name__ == '__main__':
     if(env_port is not None and env_host is not None):
