9acd389b1509e4f02c16397c747f9d7344dec182
sravanthisinha <sravani.sinha@gmail.com>
2017-10-08 22:50:49 -0700

    finish task 13

--- 0x02-restful_api_users/api/v1/auth/auth.py
+++ 0x02-restful_api_users/api/v1/auth/auth.py
@@ -12,7 +12,7 @@ class Auth():
     """A class to manage the API authentication"""
 
     def require_auth(self, path, excluded_paths):
-        """ returns True if the path is not in the list of strings excluded_paths
+        """returns True if the path is not in the list of strings excluded_paths
 
         :param path: Unknown
         :param excluded_paths: Unknown
@@ -21,7 +21,11 @@ class Auth():
         if (path is None) or (excluded_paths is None) or (not excluded_paths):
             return True
         for p in excluded_paths:
-            if (p == path) or (p[::-1][0] == '/' and p[:-1] == path):
+            if ('*' in p):
+                index = p.find('*')
+                if (p[:index] != path[:len(p[:index])]):
+                    return False
+            elif (p == path) or (p[::-1][0] == '/' and p[:-1] == path):
                 return False
         return True
 
