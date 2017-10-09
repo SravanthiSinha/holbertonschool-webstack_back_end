42ef3955e8a87be628d117bbc0fa77658acf2519
sravanthisinha <sravani.sinha@gmail.com>
2017-10-08 17:20:14 -0700

    finish task 7

--- 0x02-restful_api_users/api/v1/auth/basic_auth.py
+++ 0x02-restful_api_users/api/v1/auth/basic_auth.py
@@ -11,3 +11,17 @@ This is a BasicAuth class inside the basic_auth module.
 
 class BasicAuth(Auth):
     """A class to manage the Basic authentication"""
+
+    def extract_base64_authorization_header(self, authorization_header):
+        """returns the Base64 part of the Authorization header
+
+        :param authorization_header: contains Authorization header
+
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header.find('Basic') != 0:
+            return None
+        return authorization_header[6:]
