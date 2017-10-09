316ba33a771c837a078d7efaf3ffdcef2dd424a8
sravanthisinha <sravani.sinha@gmail.com>
2017-10-08 17:45:10 -0700

    finish task 9

--- 0x02-restful_api_users/api/v1/auth/basic_auth.py
+++ 0x02-restful_api_users/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import request
 from api.v1.auth.auth import Auth
+import base64
 """
 This is the basic_auth module.
 This is a BasicAuth class inside the basic_auth module.
@@ -25,3 +26,38 @@ class BasicAuth(Auth):
         if authorization_header.find('Basic') != 0:
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header):
+        """returns the decoded value of a Base64 string
+        base64_authorization_header
+
+        :param base64_authorization_header: authorization header to be decoded
+
+        """
+
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            data = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return data
+        except Exception as e:
+            return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header):
+        """returns the user email and password from the Base64 decoded value
+
+        :param decoded_base64_authorization_header: decoded base64 str of
+        authorization header
+
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if decoded_base64_authorization_header.find(":") == -1:
+            return None, None
+        credentails = decoded_base64_authorization_header.split(":")
+        return credentails[0], credentails[1]
