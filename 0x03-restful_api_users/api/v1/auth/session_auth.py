f340822d68fe78684226cb371cc905cab8711598
sravanthisinha <sravani.sinha@gmail.com>
2017-11-01 15:39:26 -0700

    finish task 2

--- 0x03-restful_api_users/api/v1/auth/session_auth.py
+++ 0x03-restful_api_users/api/v1/auth/session_auth.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask import request
 from api.v1.auth.auth import Auth
-import base64
+from uuid import uuid4
 """
 This is the session_auth module.
 This is a SessionAuth class inside the session_auth module.
@@ -12,4 +12,19 @@ This is a SessionAuth class inside the session_auth module.
 
 class SessionAuth(Auth):
     """A class to manage the Session authentication"""
-    pass
+    user_id_by_session_id = dict()
+
+    def create_session(self, user_id=None):
+        """
+
+        :param user_id:  (Default value = None)
+
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        if self.user_id_by_session_id.get(session_id) is None:
+            self.user_id_by_session_id[session_id] = user_id
+        return session_id
