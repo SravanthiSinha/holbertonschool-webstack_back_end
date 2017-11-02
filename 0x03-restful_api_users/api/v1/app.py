d965abb0357bb27a526e65bcb5d764b28ecda7ed
sravanthisinha <sravani.sinha@gmail.com>
2017-11-01 17:21:05 -0700

    finish task 5

--- 0x03-restful_api_users/api/v1/app.py
+++ 0x03-restful_api_users/api/v1/app.py
@@ -30,9 +30,10 @@ def before_request():
     """checks valid request paths on every request and sets the
     current user """
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
+        if (auth.authorization_header(request) is None and
+                auth.session_cookie(request) is None):
             abort(401)
         request.current_user = auth.current_user(request)
         if request.current_user is None:
